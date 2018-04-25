f4e60677dd17322990872c72110ebba10bacf1ce
nb90571 <nb90571@navercorp.com>
2018-04-25 17:19:19 +0900

    hotfix_2

--- src/test/DiscounterDto.java
+++ src/test/DiscounterDto.java
@@ -53,6 +53,9 @@ public class DiscounterDto implements Discountable {
     @Override
     public long getDiscountAmt(long originAmt) {
 
+        System.out.println("11111111111111");
+        System.out.println("22222222222222");
+
 /*
         if ("RATE".equals(dtype)) {
             return originAmt * rate / 100;
